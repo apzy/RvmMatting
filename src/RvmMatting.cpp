9f70124c601870bd4bd891407497516b4607818b
zy <zyzhouyang1@outlook.com>
2023-11-16 17:21:37 +0800

    opencv视频读取

---
+++ src/RvmMatting.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	VideoCapture capture("input.mp4");
+	Mat frame;
+	while (capture.read(frame))
+	{
+		imshow("frame", frame);
+		waitKey(1);
+	}
+	return 0;
+}
+
